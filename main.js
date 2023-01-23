6bbf1624743c8399ad109e105f6f9886bf30cf18
Rosangela-Luiggi <rosangelaluiggim@gmail.com>
2023-01-22 23:32:47 -0300

    dark mode styles and events

--- main.js
+++ main.js
@@ -1,9 +1,74 @@
 const $ = (elemento) => document.querySelector(elemento);
 
+let tareas = [
+  {
+  titulo: "Estudiar html",
+  estado: "Terminado"
+  },
+  {
+  titulo: "Estudiar css",
+  estado: "En progreso"
+  },
+  {
+  titulo: "Estudiar js",
+  estado: "Pendiente"
+  },
+  {
+  titulo: "Practicar js",
+  estado: "En progreso"
+  },
+  
+  ]
+
+window.addEventListener("load", function () {
+  /* Modal */
+  const xCloseModal = $("#close-modal");
+
+  /* vistas */
+  const $containerTask = $("#task-list");
+  const $containerCreate = $("#create-task");
+  const $containerEdit = $("#edit-task");
+
+  /* Botones vistas */
+  const $btnTask = $("#btn-task");
+  const $btnCreate = $("#btn-create");
+  const $btnEdit = $("#btn-edit");
+
+  /*Modo Oscuro */
+  const $btnDarkMode = $("#btnMode");
+  let body = document.body;
+
+  /* Funciones */
+
+  /* eventos */
+  //Cambiar sección
+  $btnTask.addEventListener("click", () => {
+    $containerCreate.classList.add("hidden");
+    $containerEdit.classList.add("hidden");
+    $containerTask.classList.remove("hidden");
+  });
+
+  $btnCreate.addEventListener("click", () => {
+    $containerTask.classList.add("hidden");
+    $containerCreate.classList.remove("hidden");
+    $containerEdit.classList.add("hidden");
+  });
+
+  $btnEdit.addEventListener("click", () => {
+    $containerTask.classList.add("hidden");
+    $containerCreate.classList.add("hidden");
+    $containerEdit.classList.remove("hidden");
+  });
 
 
-window.addEventListener('load', function () {
-    const xCloseModal = $("#close-modal");
-    
-
+  /* Modo Oscuro */
+  $btnDarkMode.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+      $btnDarkMode.innerHTML =
+        '<i class="fa-regular fa-sun"></i>';
+    } else {
+      $btnDarkMode.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
+  });
 });
