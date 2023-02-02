0fd698d959aaaf12912e23943b93af92d8040ae1
Rosangela-Luiggi <rosangelaluiggim@gmail.com>
2023-02-02 00:21:12 -0300

    task input validation

--- main.js
+++ main.js
@@ -83,6 +83,78 @@ window.addEventListener("load", function () {
       }
     });
   };
+ //funcion para ordenar tareas
+ /*  let estados = ["Pendiente", "En progreso", "Terminado"];
+
+let tareasOrdenadas =()=>{
+    let listaTarea = [];
+
+    for(let e = 0; e < estados.length; e++){
+        for(let i = 0; i<tareas.length; i++){
+            if(tareas[i].estado === estados[e]){
+             listaTarea.push(tareas[i]);
+         }
+    }
+    }
+return listaTarea;
+
+} */
+
+  // funcion agregar tarea
+  //formulario
+const $from = $(".form-create");
+const $errorFrom =$("#messageError");
+// input y error titulo
+const $inputTask = $("#task");
+const $errorTask = $("#taskError");
+const $icon = $(".validation");
+// select y error estado
+const $selectCreate = $("#select-create");
+const $errorstate = $("#selectError");
+// validación
+const regEx = /^[a-zA-Z0-9-\sñáéíóúüª!:?'¡].{5,20}$/;
+let validar = false;
+
+$inputTask.addEventListener("blur", (e)=>{
+if(!$inputTask.value.trim()){
+  $errorTask.innerText = "Este campo esta vacio";
+  $icon.classList.remove("right");
+  $icon.classList.add("incorrect");
+  validar = true;
+}else if(!regEx.test($inputTask.value)){
+  $errorTask.innerText = "Titulo incorrecto, faltan caracteres";
+  $icon.classList.remove("right");
+  $icon.classList.add("incorrect");
+  validar = true;
+}else{
+  $errorTask.innerText = "Titulo valido";
+  validar = false;
+  $errorTask.classList.remove("error");
+  $errorTask.classList.add("valid");
+  $icon.classList.add("right");
+}
+
+});
+
+//evento de formulario
+
+$from.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  let error = false;
+  let allElementsFrom = $from.elements
+ for(let i = 1; i > allElementsFrom.length - 1; i++){
+  if (allElementsFrom.value === ""){
+    $errorFrom.innerText = "Todos los campos son OBLIGATORIOS"
+     error = true;
+  }
+  else{
+    $errorFrom.innerText = ""
+  }
+ }
+});
+
+
+
 
   /* eventos */
   // vistas para el filtrado
